d8a497456bb6b928deb7dede1da14da2858ecc3f
JDK <jdkranzler@yahoo.com>
2021-01-28 15:55:51 -0600

    deleter tweaks

--- group-fs-react-shopping-list-master/src/components/App/App.jsx
+++ group-fs-react-shopping-list-master/src/components/App/App.jsx
@@ -3,33 +3,36 @@ import axios from 'axios';
 import Header from '../Header/Header.jsx'
 import './App.css';
 import AddItem from '../AddItem/AddItem'
-import DeleteList from  '../DeleteList/DeleteList.jsx';
-
-/* Hooks and definitions:
-const [shopItem, postShoppingList] = useState('');
-const [itemArray, getShoppingList] = useState([]);
-shopItem = Item/object to be purchased.
-postShoppingList = Function to create shopItem Object.
-itemArray = array of shopItem (s)
-getShoppingList = function to display itemArray.
-
-*/
-const [itemArray, deleteShoppingItem] = useState();
-
-const deleteShoppingItem = (itemId) => {
-    console.log('Deleter Clicked')
-    axios({
-        method: 'DELETE',
-        url: `/list/${id}`,
-    }).then((response) => {
-        console.log(response)
-        getShoppingList();
-
-    })
-}
+import DeleteList from '../DeleteList/DeleteList.jsx';
 
 
 function App() {
+
+
+    /* Hooks and definitions:
+    const [shopItem, postShoppingList] = useState('');
+    const [itemArray, getShoppingList] = useState([]);
+    shopItem = Item/object to be purchased.
+    postShoppingList = Function to create shopItem Object.
+    itemArray = array of shopItem (s)
+    getShoppingList = function to display itemArray.
+    
+    */
+    const [itemArray, setItemArray] = useState();
+
+    const deleteShoppingItem = (id) => {
+        console.log('Deleter Clicked')
+        axios({
+            method: 'DELETE',
+            url: `/list/${id}`,
+        }).then((response) => {
+            console.log(response)
+            getShoppingList();
+
+        })
+    }
+
+
     return (
         <div className="App">
             <Header />
