80977e152fd7b1ba63a3ba3463543ffefbaadacc
Zach Mead <zachary.n.mead@gmail.com>
2021-01-28 16:34:05 -0600

    Working on GET/rendering to DOM

--- group-fs-react-shopping-list-master/src/components/ShoppingList/ShoppingList.jsx
+++ group-fs-react-shopping-list-master/src/components/ShoppingList/ShoppingList.jsx
@@ -16,26 +16,21 @@ function ShoppingList({ shoppingList }) {
     return (
         <>
         {shoppingList.map((item) => 
-        <div className="items"> 
+        <div key={item.id}className="items"> 
             {/* {JSON.stringify(props)} */}
-            <h2>{item.name}</h2>
+            
             <p>Item: {item.name}</p>
 
-            <h2>{item.itemQuantity}</h2>
             <p>Quantity: {item.quantity}</p>
 
-            <h2>{item.itemUnit}</h2>
             <p>Unit: {item.unit}</p>
 
-            <button onClick={() => shoppingList(item.id)}>Purchase</button>
-            <button onClick={() => deleteShoppingItem(props.id)} class='deleter'> Delete </button>
+           {/* <button onClick={() => shoppingList(item.id)}>Purchase</button>*
+            <button onClick={() => deleteShoppingItem(props.id)} class='deleter'> Delete </button>*/}
         </div>
         )}
         </>
     );
-
-
-
 }
 
 export default ShoppingList;
