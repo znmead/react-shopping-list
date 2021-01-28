37c496f626c939ee0a267aef9039e76352efc099
LanceLRG <Gagnerlance@gmail.com>
2021-01-28 14:23:17 -0600

    setup POST route and lost half my hair

--- group-fs-react-shopping-list-master/server/routes/list.router.js
+++ group-fs-react-shopping-list-master/server/routes/list.router.js
@@ -8,6 +8,21 @@ const pool = require('../modules/pool.js');
 // GET 
 
 // POST
+router.post('/', (req, res) => {
+    console.log(`req body is: `,req.body);
+    const newItem = req.body;
+    const sqlText = `INSERT INTO list (name, quantity, unit)
+                     VALUES ($1, $2, $3)`;
+    pool.query(sqlText, [newItem.name, newItem.quantity, newItem.unit])
+        .then((result) => {
+            console.log(`Added New item to the database`, newItem);
+            res.sendStatus(201);
+        })
+        .catch((error) => {
+            console.log(`Error making database query ${sqlText}`, error);
+            res.sendStatus(500); // Good server always responds
+        })
+})
 
 // PUT - URL list
 
