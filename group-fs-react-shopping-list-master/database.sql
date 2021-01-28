180c4be4dd57a73768155b259942f5993f5b8501
Zach Mead <zachary.n.mead@gmail.com>
2021-01-28 11:08:26 -0600

    Initial commit/db setup

--- group-fs-react-shopping-list-master/database.sql
+++ group-fs-react-shopping-list-master/database.sql
@@ -1,2 +1,18 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+-- Table structure
+CREATE TABLE "list" (
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR(80) NOT NULL,
+	"quantity" INTEGER NOT NULL,
+    "unit" VARCHAR (20) NOT NULL,
+    "purchased" BOOLEAN DEFAULT 'false'
+);
+
+INSERT INTO list (name, quantity, unit)
+VALUES ('Apples', '2', 'basket'), 
+('Milk', '1', 'gallon'), 
+('Eggs', '1', 'Dozen'),
+('Coffee', '3', 'pounds'),
+('Bacon', '5', 'pounds')
