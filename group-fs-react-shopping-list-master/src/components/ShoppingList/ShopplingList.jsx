953e1ee208e4cd789df92b61fb376275800a1c48
Zach Mead <zachary.n.mead@gmail.com>
2021-01-28 14:28:46 -0600

    Working on adding items to DOM

--- group-fs-react-shopping-list-master/src/components/ShoppingList/ShopplingList.jsx
+++ group-fs-react-shopping-list-master/src/components/ShoppingList/ShopplingList.jsx
@@ -1,2 +1,28 @@
-// This is the GET display.
+// This is the GET display
 
+import ShoppingListItem from '../ShoppingListItem';
+
+function ShoppingList(props) {
+
+
+    // put our list of items on the DOM
+    // JS
+    let listItems = props.list.map((item) => {
+        // return some JSX for EVERY item in the itemList
+        return (
+            <ShoppingListItem
+                item={item}
+                itemsAssemble={props.itemsAssemble} />
+        )
+    });
+
+    // JSX on the DOM
+    return (
+        <div>
+            {itemList}
+        </div>
+    );
+
+}
+
+export default ShoppingList;
